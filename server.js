be9fad2e790f24254b167f6bb94b9881db1243b0
Marcelo <marcesotero@yahoo.com>
2018-07-11 21:15:04 -0300

    Add view for cool eyes

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ var app = express()
 hbs.registerPartials(path.join(__dirname, 'views/partials'))
 app.set('view engine', 'hbs')
 
-hbs.registerHelper('getCurrentYear', () => {return new Date().getFullYear()})
-hbs.registerHelper('screamIt', (text) => {return text.toUpperCase()})
+hbs.registerHelper('getCurrentYear', () => { return new Date().getFullYear() })
+hbs.registerHelper('screamIt', (text) => { return text.toUpperCase() })
+hbs.registerHelper('cool', (text) => { return cool() })
 app.use((req, res, next) => {
   var now = new Date().toString()
   var log = `${now} ${req.method} ${req.url}`
@@ -21,38 +22,33 @@ app.use((req, res, next) => {
   })
   next()
 })
-// app.use((req, res, next) => {
-//   res.render('maintenance.hbs')
-// })
 app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => {
-  // res.send({
-  //   name: 'Marcelo',
-  //   likes: ['Bike', 'Cities']
-  // })
   res.render('index.hbs', {
     pageTitle: 'Home Page',
     welcomeMessage: 'Welcome to my site'
   })
 })
 app.get('/about', (req, res) => {
-  //res.send('about page')
   res.render('about.hbs', {
     pageTitle: 'About Page'
   })
 })
 app.get('/bad', (req, res) => {
   res.send({
-    error:'unable to fullfil your request'
+    error: 'unable to fullfil your request'
   })
 })
 app.get('/projects', (req, res) => {
-  //res.send('about page')
   res.render('projects.hbs', {
     pageTitle: 'Projects Page'
   })
 })
-app.get('/cool', (req, res) => res.send(cool()))
+app.get('/cool', (req, res) => {
+  res.render('cool.hbs', {
+    pageTitle: 'Projects Page'
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`Server is up on port ${PORT}`)
